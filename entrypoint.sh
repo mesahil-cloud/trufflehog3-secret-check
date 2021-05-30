6a381d34c9fd892501c4f4d14a346dccd1fda970
saharora <60168593+saharora@users.noreply.github.com>
2021-05-31 01:41:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ fi
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
 # which is mounted into our image. This means, as long as a checkout action was
 # done before our action runs, we'll have access to the repository.
-githubRepo="file://$(pwd)" # Default target repository
+#githubRepo="file://$(pwd)" # Default target repository
 query="$args" # Build args query with repository url
-trufflehog3 $query $githubRepo
+trufflehog3 $query .
